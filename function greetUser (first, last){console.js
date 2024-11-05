c7c6c34ef65d4847c6fd623699a77a777336d926
monefifita460 <saimonekfifita@gmail.com>
2024-11-05 15:42:28 +1300

    t git commit .

---
+++ function greetUser (first, last){console.js
@@ -0,0 +1,10 @@
+function greetUser (first, last){console. log(`hello ${first} ${last}`)}
+greetUser("john","doe")
+
+
+
+function multiply(a, b) {
+  return a * b;
+}
+=
+const multiply = (a,b) => {return a * b}
